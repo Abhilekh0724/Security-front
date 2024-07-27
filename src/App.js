fa6d94d55d2b66439380f58a0d84eb4d0ebbf78c
Abhilekh0724 <nimwang0724@gmail.com>
2024-07-27 22:18:37 +0545

    admin

--- src/App.js
+++ src/App.js
@@ -19,23 +19,22 @@ import AdminCategoryList from "./pages/admin/AdminPanel";
 function App() {
   return (
     <Router>
-      {/* <Loginpage/> */}
       <Navbar />
       <ToastContainer />
       <Routes> 
         <Route path="/homepage" element={<Homepage />} /> 
         <Route path="/profile" element={<Profile />} /> 
         <Route path="/searchpage" element={<SearchResults />} />
-        <Route path="/category" element={<CelebrationHalls />} />
-        <Route path="/category" element={<Photographer />} />
-        <Route path="/category" element={<MakeupArtist />} />
-        <Route path="/category" element={<Venue />} />
+        <Route path="/category/celebration-halls" element={<CelebrationHalls />} />
+        <Route path="/category/photographers" element={<Photographer />} />
+        <Route path="/category/makeup-artists" element={<MakeupArtist />} />
+        <Route path="/category/venues" element={<Venue />} />
         <Route path="/search" element={<SearchResults />} />
-        <Route path="/nabbar" element={<Navbar />} /> 
+        <Route path="/navbar" element={<Navbar />} /> 
         <Route path="/admin/category" element={<AdminCategoryDashboard />} />
         <Route path="/admin/list" element={<AdminCategoryList />} />
         <Route path="/login" element={<Loginpage />} />
-        <Route path="/categoryDetail" element={<CategoryDetail />} />
+        <Route path="/category/:id" element={<CategoryDetail />} />
         <Route path="/register" element={<Registerpage />} />
       </Routes>
     </Router>
